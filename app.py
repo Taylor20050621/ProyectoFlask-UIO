3be9dd03da4dc7fe653ef8eb51abbaca5405eeba
Diego Yanez <alexanderyanez21@gmail.com>
2026-05-06 09:56:18 -0500

    conexion base de datos

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+# 1. CARGAR VARIABLES DE ENTRONO DESDE .env (PRIMERA LINEA DE PROTECCION)
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+# 2. CONSTRUCCION DIDADCTICA DE LA CONEXION CON POSTGRES
 
-if __name__ == "__main__":
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+# LA RECETA DE LA URI PARA CONECTARSE
+SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# 3. INSTANCIA DE SQLALCHEMY (LA LLAVEMAESTRA)
+db = SQLAlchemy(app)
+
+@app.route("/")
+def test_connection():
+    return jsonify({
+        "status": "online",
+        "message": "Configuración de base de datos cargada correctamente",
+        "user_connected": DB_USER
+    })
+
+
+if __name__ == '__main__':
     app.run(debug=True)
